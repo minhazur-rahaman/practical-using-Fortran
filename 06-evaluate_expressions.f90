a02984451f1d9d288907e40ffd2dee512515b8f5
Mohammad Minhazur Rahaman <117487615+minhazur-rahaman@users.noreply.github.com>
2024-11-04 23:14:54 -0800

    Update 06-evaluate_expressions.f90

--- 06-evaluate_expressions.f90
+++ 06-evaluate_expressions.f90
@@ -1,27 +1,36 @@
 program evaluate_expressions
-IMPLICIT NONE
-real ::a,b,c,d,r,x,u,v
-integer :: serial_num
+    IMPLICIT NONE
+    real :: a, b, c, d, r, x, u, v
+    integer :: serial_num
 
-do
-print*, "Enter the serial number: "
-read*, serial_num
-if (serial_num == 0) EXIT
+    do
+        print*, "Enter the serial number (0 to exit): "
+        read*, serial_num
+        if (serial_num == 0) EXIT
 
-print*, "Type the value of a, b, r, d :" 
-read*, a, b, d, r
-if (r==d .and. b==0) THEN
-print*, "the equation undefine"
+        print*, "Type the values of a, b, r, d :"
+        read*, a, b, r, d
 
-else 
-u =	abs(a+(1/b))/sin(r-d)
-print*, "value of U is : ",u 
-print*, "Type the value of c, x :	"
-read*, c, x
-v = (c*(1/u)-u*cos(x))/b
-print*, "value of V is : ",v
+        ! Check for undefined conditions in u calculation
+        if (r == d .or. b == 0) THEN
+            print*, "The equation for U is undefined due to b=0 or (r=d)."
+            cycle  ! Go to the next iteration if u is undefined
 
-end if
-end do 
+        else 
+            u = abs(a + (1.0 / b)) / sin(r - d)
+            print*, "Value of U is:", u
+
+            print*, "Type the values of c and x:"
+            read*, c, x
+
+            ! Check for undefined conditions in v calculation
+            if (u == 0) then
+                print*, "The equation for V is undefined because u = 0."
+            else 
+                v = (c * (1.0 / u) - u * cos(x)) / b
+                print*, "Value of V is:", v
+            end if
+        end if
+    end do
 
 end program evaluate_expressions
